dfb57a859e7cad9d7440f1288f1fece0404514b3
manjul000 <manjuldahal1@gmail.com>
2022-08-12 20:27:42 +0545

    New line

--- any_any.py
+++ any_any.py
@@ -2,4 +2,10 @@
 
 '''
 My name is Manjul Dahal. I am currently a student at softwarica college.
+This is an example of multi line comment.
 '''
+
+"""
+You can also use double quotes instead of single to comment multiple lines out.
+This was added one day later.
+"""
